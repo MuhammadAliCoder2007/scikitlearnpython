32fa4aa65eab76b5bd20456293a0b8c008636fe1
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-06-15 13:56:25 -0400

    update

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import sklearn
+%pip install --upgrade scikit-learn==0.23.0
+from sklearn.datasets import load_boston
+X,y = load_boston(return_X_y=True)
+from sklearn.neigbors import KNeighborsRegressor
+mod = KNeighborsRegressor()
+mod.fit(X,y)
